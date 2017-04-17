9d099ffda318be061bc05d52c4c59f20893d2e7a
William Cheung <catdoublepipe@gmail.com>
2017-04-17 01:00:04 +0100

    PluralSight Module 1 cont.

--- ps-rxjs/main.ts
+++ ps-rxjs/main.ts
@@ -1,3 +1,27 @@
-/**
- * Created by denisecheung on 16/04/2017.
- */
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+
+let numbers = [1, 5, 10];
+let source = Observable.create(observer => {
+
+  let index = 0;
+  let produceValue = () => {
+    observer.next(numbers[index++]);
+
+    if (index < numbers.length) {
+      setTimeout(produceValue, 500);
+    } else {
+      observer.complete();
+    }
+  }
+
+  produceValue();
+}).map(x => x * 2)
+  .filter(x => x > 4);
+
+source.subscribe(
+  value => console.log(`value: ${value}`),
+  err => console.error(`error: ${err}`),
+  () => console.log('done')
+);
